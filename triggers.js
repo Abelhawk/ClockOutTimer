47f5bcffc3213fa4ad8e3b7d2ab649535a5ade7c
Austin Ballard <abelhawk@gmail.com>
2018-10-19 12:30:25 -0600

    Now mostly programmed... need to work on less than 40 hours version though

--- triggers.js
+++ triggers.js
@@ -1,27 +1,29 @@
-window.onload = initialize(); startTime();
+let todaysDate = new Date();
+
+window.onload = initialize();
+startTime();
 
 function initialize() {
     console.log("Testing");
     let todaysDate = new Date;
     let today = dayOfTheWeek(todaysDate.getDay());
     let todayDiv = document.getElementById('dayOfTheWeek');
-    if (today !== 'Saturday' && today !== 'Sunday'){
+    if (today !== 'Saturday' && today !== 'Sunday') {
         todayDiv.innerText = 'Happy ' + today + '!'
-
     } else {
         todayDiv.innerText = 'Working weekends, eh? :/'
     }
 }
 
 function startTime() {
-    let today = new Date();
-    let h = today.getHours();
-    let m = today.getMinutes();
-    let s = today.getSeconds();
+    todaysDate = new Date();
+    let h = todaysDate.getHours();
+    let m = todaysDate.getMinutes();
+    let s = todaysDate.getSeconds();
     let ampm;
-    if (h >= 13){
+    if (h >= 12) {
         ampm = 'p.m.';
-        h -= 12;
+        if (h !== 12) h -= 12;
     } else {
         ampm = 'a.m.'
     }
@@ -31,20 +33,22 @@ function startTime() {
         h + ":" + m + ":" + s + ' ' + ampm;
     let t = setTimeout(startTime, 500);
 }
+
 function checkTime(i) {
-    if (i < 10) {i = "0" + i} // add zero in front of numbers < 10
+    if (i < 10) {
+        i = "0" + i
+    } // add zero in front of numbers < 10
     return i;
 }
 
 function validatePassKey(tb) {
-    console.log("Woo");
-    if (tb.value.length >= 2){
+    if (tb.value.length >= 2) {
         document.activeElement.blur();
         document.getElementById('minutes').focus();
     }
 }
 
-function clearField(input){
+function clearField(input) {
     input.value = '';
 }
 
@@ -65,4 +69,42 @@ function dayOfTheWeek(day) {
         case 6:
             return 'Saturday';
     }
+}
+
+function calcHours() {
+    todaysDate = new Date();
+    let hoursLeft = 40;
+    let ampm;
+    let div = document.getElementById('output');
+    let currentTime = document.getElementById('timeClockedIn').value;
+    let hoursLogged = parseInt(document.getElementById('hours').value);
+    let minutesLogged = parseInt(document.getElementById('minutes').value);
+    if (hoursLogged >= 40) hoursLeft = 80;
+    let hourMins = hoursLeft * 60;
+    let currentHourMins = parseInt(currentTime.substr(0, 2)) * 60 + parseInt(currentTime.substr(3, 2));
+    let loggedHourMins = hoursLogged * 60 + minutesLogged;
+    let result = hourMins - loggedHourMins;
+    let x = (result + currentHourMins) / 60;
+    let xMinutes = Math.round((x % 1) * 60);
+    let xHours = x - (x % 1);
+    if (xHours >= 12) ampm = 'PM';
+    else ampm = 'AM';
+    let clockOutTime = new Date(todaysDate.getFullYear(), todaysDate.getMonth(), todaysDate.getDate(), xHours, xMinutes, 0);
+    console.log(clockOutTime);
+    if (xMinutes < 10) xMinutes = '0' + xMinutes;
+    if (xHours > 12) xHours -= 12;
+    if (Math.sign(todaysDate - clockOutTime) === -1) {
+        div.style.backgroundColor = '#b9cfe5';
+        div.innerHTML = `<h3>You should clock out today at:</h3><p class="timeDisplay">
+        ${xHours}:${xMinutes} ${ampm}
+    </p>`;
+    } else {
+        div.style.backgroundColor = 'lightcoral';
+        div.innerHTML = `<h3>Good heavens, you're over on time! Clock out <span class="italic">now,</span> you maniac! Go! <span class="italic">GO!!</span></h3>`
+    }
+
+}
+
+function rando(n) {
+    return Math.floor(Math.random() * n + 1)
 }
